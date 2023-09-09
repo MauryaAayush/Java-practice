1ce57797e270a6257be8490b35fad3bbc0c7850f
Aayush <maayush235@gmail.com>
2023-09-09 21:54:13 +0530

    09/09/2023

--- src/Pr1.java
+++ src/Pr1.java
@@ -1,6 +1,24 @@
+package src;
+
+import java.util.Scanner;
+
 public class Pr1 {
     public static void main(String[] args)
     {
-        System.out.println("Addition");
+        Scanner input = new Scanner(System.in);
+
+        short a,b;
+
+
+        System.out.print("Enter the value of length (a) : ");
+        a = input.nextShort();
+
+        System.out.print("Enter the value of breadth (b) : ");
+        b = input.nextShort();
+
+        System.out.print("Here is your area : "+a*b+" | here is your perimeter"+ (2*(a+b)) );
+
+
+
     }
 }
